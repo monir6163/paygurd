4047b78cca10782a5f5bbf2342b76376b88feda0
monir6163 <monirhossain6163@gmail.com>
2025-01-13 02:06:23 +0600

    v-5

--- src/app/actions/login.ts
+++ src/app/actions/login.ts
@@ -17,7 +17,7 @@ export async function login(credintial: any) {
   const { error, data } = await supabase.auth.signInWithPassword(authData);
 
   if (error) {
-    return { error };
+    return { error: error.message };
   }
   //check existing user
   const { data: existingUser } = await supabase
